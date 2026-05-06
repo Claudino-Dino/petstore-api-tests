ab2c1deef65f9fa44416ceafba49920ba86d0681
Dino <138309491+Claudino-Dino@users.noreply.github.com>
2026-05-05 23:55:52 -0300

    Enhance login method with WebDriverWait
    Replaced direct element access with WebDriverWait to ensure elements are present and clickable before interaction.

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,21 +1,21 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
-
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def fazer_login(self, email, senha):
-        self.driver.find_element(By.XPATH,"//*[@id='user-name']").send_keys(email)
-        self.driver.find_element(By.XPATH,"//*[@id='password']").send_keys(senha)
-        self.driver.find_element(By.XPATH   ,"//*[@id='login-button']").click()
-
+        self.wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='user-name']"))).send_keys(email)
+        self.wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='password']"))).send_keys(senha)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='login-button']"))).click()
+        self.wait.until(EC.url_to_be("https://www.saucedemo.com/inventory.html"))
         url_carrinho_page = "https://www.saucedemo.com/inventory.html"
-
-        if (self.driver.current_url==url_carrinho_page):
+        if self.driver.current_url == url_carrinho_page:
             print("""
 -----------------------------------
     LOGIN EFETUADO ✅
 -----------------------------------
                 """)
-
